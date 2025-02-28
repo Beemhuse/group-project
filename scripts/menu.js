10d8f9dadf71cea8b6da7e5b5a6cc8f0ec2d67d5
charishub <charitykosisochukwu33@gmail.com>
2025-02-28 12:19:48 +0100

    update

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,10 +1,40 @@
-const dishButton = document.querySelectorAll(".available-dishes");
 
-dishButton.forEach((button) => {
-    button.addEventListener("click", (e) => {
-        e.preventDefault();
-        let dishes = document.getElementById("rice-dishes")
-        dishes.style.display = "flex";
+const menuUrl = './pages/menu.json';
+let data = []
 
-    });
+document.addEventListener("DOMContentLoaded", (e) => {
+    e.preventDefault();
+    let dishButton = document.querySelectorAll(".available-dishes");
+    dishButton.forEach((dishButton) => {
+        dishButton.addEventListener("click", async (e) => {
+            e.preventDefault();
+            if (e.target.dataset.category === "Rice Dishes") {
+                try {
+                    const response = await fetch(menuUrl);
+                    data = await response.json();
+                    const riceDishes = data.filter(item => item.category === "Rice Dishes");
+                
+                    const riceDishesContainer = document.getElementById('rice-dishes');
+                    // riceDishesContainer.innerHTML = "";
+                    riceDishes.forEach((dish) => {
+                        const riceMenu = document.createElement("div");
+                        riceMenu.classList.add("rice-menu");
+                        riceMenu.innerHTML = `
+                                        <p>${dish.name}</p>
+                                        <p>${dish.description}</p>
+                                        <img src="${dish.image}"/>
+                                        <p>$${dish.price}</p>`
+                        riceDishesContainer.appendChild(riceMenu);
+                    })
+                } catch (error) {
+                    console.error('Error:', error);
+
+                }
+
+            }
+        })
+    })
 })
+
+
+
