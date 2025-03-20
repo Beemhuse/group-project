52121069ff4f925dfeaf07bde67649614cc850cf
sparks042 <chinedufreedom10@gmail.com>
2025-03-20 14:02:22 +0100

    pulling from dev

--- scripts/verifyAcess.js
+++ scripts/verifyAcess.js
@@ -1,20 +1,20 @@
-const loginButton = document.getElementById("log-in-btn");
+// const loginButton = document.getElementById("log-in-btn");
 
-document.addEventListener("DOMContentLoaded", () => {
-    const token = sessionStorage.getItem("token");
-    if(token){
-        loginButton.style.display = "none";
+// document.addEventListener("DOMContentLoaded", () => {
+//     const token = sessionStorage.getItem("token");
+//     if(token){
+//         loginButton.style.display = "none";
 
-    }
+//     }
   
-    })
+//     })
 
     document.addEventListener("DOMContentLoaded", () => {
         const token = localStorage.getItem('authToken');
         
         if (token) {
             // If the user is logged in, hide login button and show user icon
-            const loginButton = document.getElementById('login-btn');  // assuming this exists on the dashboard page
+            const loginButton = document.querySelector('.log-in-button');  // assuming this exists on the dashboard page
             
             if (loginButton) {
                 loginButton.style.display = 'none'; // Hide the login button
